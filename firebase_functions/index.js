f66dcf8c89cf6343f324bd11ed1a85bec8b7357c
Nathaniel Johnson <nathancj224@gmail.com>
2017-05-09 11:24:27 -0400

    Cleaned up firebase contact function and added gitkeep to the keys directory.

--- firebase_functions/index.js
+++ firebase_functions/index.js
@@ -1,34 +1,22 @@
 /**
- * Copyright 2015 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Contact form email notification
  */
+
 'use strict';
 
 const functions = require('firebase-functions');
 const nodemailer = require('nodemailer');
-// Configure the email transport using the default SMTP transport and a GMail account.
-// For other types of transports such as Sendgrid see https://nodemailer.com/transports/
-// TODO: Configure the `gmail.email` and `gmail.password` Google Cloud environment variables.
 const gmailEmail = encodeURIComponent(functions.config().gmail.email);
 const gmailPassword = encodeURIComponent(functions.config().gmail.password);
 const mailTransport = nodemailer.createTransport(
     `smtps://${gmailEmail}:${gmailPassword}@smtp.gmail.com`);
 
-// Sends an email confirmation when a user changes his mailing list subscription.
-exports.sendEmail = functions.database.ref('/contact/{id}').onWrite(event => {
+// Sends an email when contact form is submitted
+exports.contactFormSendEmail = functions.database.ref('/contact/{id}').onWrite(event => {
   const snapshot = event.data;
   const val = snapshot.val();
+
+  console.log('Contact form submitted by: ' + val.email);
 
   const mailOptions = {
     from: '"LightUpBurton.com" <nathancj224@gmail.com>',
@@ -36,15 +24,13 @@ exports.sendEmail = functions.database.ref('/contact/{id}').onWrite(event => {
     to: 'nathancj224@gmail.com'
   };
 
-  var text = 'Someone has submitted the contact form on LightUpBurton.com: \n\n' +
+  mailOptions.subject = 'LightUpBurton.com Contact Form [#' + val.timestamp +']';
+  mailOptions.text = 'Someone has submitted the contact form on LightUpBurton.com: \n\n' +
   'Name: ' + val.name + '\n' +
   'Email: ' + val.email + '\n' +
   'Message: \n' + val.message + '\n';
-
-  mailOptions.subject = 'LightUpBurton.com Contact Form [#' + val.timestamp +']';
-  mailOptions.text = text;
   return mailTransport.sendMail(mailOptions).then(() => {
-    console.log('Sent message');
+    console.log('Contact form email has been sent.');
   });
 
 });
